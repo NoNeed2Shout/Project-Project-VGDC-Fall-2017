05afe02a62ce4234fc1af71d469a6605cafb9b4b
NoNeed2Shout <hjefamily@yahoo.com>
2017-10-31 18:07:45 -0700

    Started on scripts for projectile, rook-themed boss
    I'll be referring to the bullet hell stuff I made to make the projectiles work. Will probably refer to it for how to make hitboxes and such things too.

---
+++ ProjectProjectVGDC2017/Assets/Scripts/RookBoss.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RookBoss : MonoBehaviour {
+    //not used to bosses rotating between patterns so my current idea is that the int for pattern determines the pattern currently used
+    //timer is how much time left on the pattern, and if there are specific actions (eg projectiles spawning) then they spawn on
+    //timer%something
+
+    //I'm numbering phases in the order I create them, I guess. phase 1: aimed projectiles. phase 2: move side-to-side, unaimed projectiles
+    
+    //put in a berserk bool in case we want to make the boss have a low hp rage mode or desperation attack
+    private int health=100,maxHealth=100,timer,pattern;
+    private bool moving=false,berserk=false;
+    private float moveSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (pattern == 1)
+        {
+        }
+        else if (pattern == 2)
+        {
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //stuff about boss getting hit by sword, triggering contact damage to player goes here
+        //projectiles created by boss will need to have their own script, sprite
+    }
+}
