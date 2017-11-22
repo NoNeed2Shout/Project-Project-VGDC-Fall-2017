e8568de772dc5913a2a61ad37edac8c76cbee9c5
NoNeed2Shout <hjefamily@yahoo.com>
2017-11-21 18:05:40 -0800

    Dumped ideas for a ramming attack into an empty script

---
+++ ProjectProjectVGDC2017/Assets/Scripts/Ram.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ram : MonoBehaviour {
+
+    //Should contain variables for a straightforward ramming attack. Maybe also adapt it to one where ramming a boundary spawns some projectiles.
+    //Key for simple ramming: timer while keeping track of player (maybe make it stationary and rotating to face player for variety)
+    //Include a visual cue before charge (should be a quick dash) as a "get out of the way" - probably same as the laser's?
+    //Boss should be immobile for a little bit after end of charge (probably when hitting a wall) before rotating itself to default and moving back up
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
